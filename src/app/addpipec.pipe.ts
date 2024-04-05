bfbdde9c837dcf8224187f6088b9c994d44333cd
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-04-05 10:51:26 -0400

    Add Array and Attached Currency Symbol

---
+++ src/app/addpipec.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'addpipec'
+})
+export class AddpipecPipe implements PipeTransform {
+  transform(arr:number[]) {
+    
+    const sum = arr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    return sum;
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddpipecPipe } from './addpipec.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddpipecPipe
   ],
   imports: [
     BrowserModule
